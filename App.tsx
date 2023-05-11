fc08be5fad8c1a6ca43276123d81d5d2be16c204
santvh11 <santvh11@gmail.com>
2023-05-11 16:14:25 -0500

    add splash screen on ios

--- App.tsx
+++ App.tsx
@@ -5,47 +5,16 @@
  * @format
  */
 
-import React from 'react';
-import {
-  FlatList,
-  SafeAreaView,
-  Text,
-  TouchableOpacity,
-  View,
-} from 'react-native';
-import {useGetCountries} from './src/hooks/useGetCountries';
+import React, {useEffect} from 'react';
+import SplashScreen from 'react-native-splash-screen';
+import CountriesList from './src/components/countriesList/CountriesList';
 
 function App(): JSX.Element {
-  const {countries} = useGetCountries();
+  useEffect(() => {
+    SplashScreen.hide();
+  }, []);
 
-  const ItemSeparatorComponent = () => <View style={{height: 20}} />;
-
-  return (
-    <SafeAreaView style={{flex: 1, justifyContent: 'center'}}>
-      <FlatList
-        data={countries}
-        ItemSeparatorComponent={ItemSeparatorComponent}
-        renderItem={({item}) => (
-          <View style={{flexDirection: 'row', justifyContent: 'space-between'}}>
-            <TouchableOpacity
-              style={{
-                width: 50,
-                backgroundColor: 'red',
-                justifyContent: 'center',
-                alignItems: 'center',
-              }}>
-              <Text>GO</Text>
-            </TouchableOpacity>
-            <View style={{flex: 1, paddingHorizontal: 20}}>
-              <Text>{item.name}</Text>
-              <Text>{item.capital}</Text>
-              <Text>{item.region}</Text>
-            </View>
-          </View>
-        )}
-      />
-    </SafeAreaView>
-  );
+  return <CountriesList />;
 }
 
 export default App;
